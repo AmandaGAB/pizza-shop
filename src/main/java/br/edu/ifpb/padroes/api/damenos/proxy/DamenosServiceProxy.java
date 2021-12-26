4951b2a97bdd9f353bf8bc60c201b46b20d06cbc
AmandaGAB <amandagabsa@gmail.com>
2021-12-26 18:35:39 -0300

    padrao proxy

--- src/main/java/br/edu/ifpb/padroes/api/damenos/proxy/DamenosServiceProxy.java
+++ src/main/java/br/edu/ifpb/padroes/api/damenos/proxy/DamenosServiceProxy.java
@@ -1,14 +1,31 @@
 package br.edu.ifpb.padroes.api.damenos.proxy;
 
 import br.edu.ifpb.padroes.api.damenos.DamenosPizza;
+import br.edu.ifpb.padroes.api.damenos.DamenosServiceImpl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // TODO - implementar proxy de cache para evitar leitura do json a cada chamada do método
 public class DamenosServiceProxy implements DamenosService {
+
+    private DamenosService damenosService;
+    List<DamenosPizza> cacheall = new ArrayList<>();
+
+
+    public void DamenosCacheProxy(){
+        this.damenosService = new DamenosServiceImpl();
+    }
+
+
     @Override
     public List<DamenosPizza> getPizzas() {
         // TODO - implementar proxy
-        return null;
+        if(cacheall.isEmpty()){
+            cacheall = damenosService.getPizzas();
+        }else{
+            System.out.println("Lista recuperada do cache");
+        }
+        return cacheall;
     }
 }
